54a982aa742e7d2a6676bbc1a19109634259ff0d
AlijonovaArofatoy <116580493+AlijonovaArofatoy@users.noreply.github.com>
2022-11-02 22:47:12 +0500

    Add files via upload

---
+++ a3.1 tajriba ishi.py
@@ -0,0 +1,11 @@
+import math
+from cmath import cos,exp, sin
+import math
+x=1
+y=1
+z=3
+e=2.71
+a=(1+y)*(x+y/(x*x+4))/(pow(e,(-x-2))+1/(x*x+4))
+b=(1+cos(y-2))/(pow(x,4)/2+(pow(sin(z),2)))
+print('a=',a)
+print('b=',b)
